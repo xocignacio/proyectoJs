ee491bf93f76ccce5009ed0802be9cf9cf8695e6
xocignacio <xose_ignacio@hotmail.com>
2021-12-03 19:31:38 -0300

    repo para el 3er desafio

---
+++ Js/main.js
@@ -0,0 +1,50 @@
+// /* PRIMER DESAFIO = Crear un algoritmo JS simple*/ 
+
+// let nombre = prompt ('cual es tu nombre?')
+
+// if (nombre) {
+//      alert('hola' + nombre + ' Bienvenido a mi pagina web')
+// }
+
+// /* SEGUNDO DESAFIO =  crear un algoritmo con un condicional */
+
+/*let nombre = prompt ('cual es tu nombre?')
+let edad = +prompt('cuantos años tenes?')
+
+console.log(nombre, typeof nombre)
+console.log(edad, typeof edad)
+
+if (edad <= 18) {
+   alert('Perdon, pero para ingresar a mi web debes ser mayor')
+ } else {
+  alert('Bienvenido a mi pagina web')
+}
+
+/*/
+
+// /* TERCER DESAFIO =  Simulador interactivo */
+
+let entrada = prompt ('Select language => español/english ')
+switch (entrada) {
+  case "español":
+    alert('Bienvenido a mi pagina web');
+    break;
+  case "english":
+    alert('Welcome to my WebSite')
+    break;
+  case 'spanish':
+    alert('Bienvenido a mi pagina web');
+    break;
+  case "ingles":
+    alert('Welcome to my WebSite')
+    break;
+  case "ESPAÑOL":
+    alert('Bienvenido a mi pagina web');
+    break;
+  case "ENGLISH":
+    alert('Welcome to my WebSite')
+    break;
+  default:
+    alert('Idioma seleccionado incorrecto')
+    alert('Igualmente, bienvenido...')
+}
